aa072538a0a95e2fc99fa9225433e72bf64f8177
Jonathan Deronja <wwilliamsonderonja01@qub.ac.uk>
2024-07-12 00:39:24 +0100

    Updating To EMAILJS

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 
 const express = require('express');
 const bodyParser = require('body-parser');
-const nodemailer = require('nodemailer');
+
 const app = express();
 const port = 3000;
 
@@ -21,50 +21,11 @@ app.use(bodyParser.json());
 
 // Define routes
 app.get('/', (req, res) => {
-    res.render('index', { title: 'Rose and Co. Beauty Salon Marketing' });
-});
-
-app.post('/contact', (req, res) => {
-    const { firstName, lastName, email, phone, companyName, position } = req.body;
-
-    // Create a transporter object using the default SMTP transport
-    let transporter = nodemailer.createTransport({
-        service: 'gmail', // Use your email service provider
-        auth: {
-            user: process.env.EMAIL_USER, // Use environment variables
-            pass: process.env.EMAIL_PASS  // Use environment variables
-        }
-    });
-
-    // Setup email data with unicode symbols
-    let mailOptions = {
-        from: `"Rose and Co." <${process.env.EMAIL_USER}>`, // Sender address
-        to: 'destination-email@gmail.com', // List of receivers
-        subject: 'New Contact Request', // Subject line
-        text: `You have a new contact request:
-        Name: ${firstName} ${lastName}
-        Email: ${email}
-        Phone: ${phone}
-        Company: ${companyName}
-        Position: ${position}`, // Plain text body
-        html: `<p>You have a new contact request:</p>
-               <ul>
-                   <li>Name: ${firstName} ${lastName}</li>
-                   <li>Email: ${email}</li>
-                   <li>Phone: ${phone}</li>
-                   <li>Company: ${companyName}</li>
-                   <li>Position: ${position}</li>
-               </ul>` // HTML body
-    };
-
-    // Send mail with defined transport object
-    transporter.sendMail(mailOptions, (error, info) => {
-        if (error) {
-            return console.log(error);
-        }
-        console.log('Message sent: %s', info.messageId);
-        // Redirect or respond with a success message
-        res.redirect('/'); // Redirect to home or show a success message
+    res.render('index', {
+        title: 'Rose and Co. Beauty Salon Marketing',
+        emailjs_user_id: process.env.EMAILJS_USER_ID,
+        emailjs_service_id: process.env.EMAILJS_SERVICE_ID,
+        emailjs_template_id: process.env.EMAILJS_TEMPLATE_ID
     });
 });
 
